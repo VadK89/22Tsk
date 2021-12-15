9d9b1e123372eefcf3c0ae1031af30157d05ce2e
Вадим <Vadimk89@mail.ru>
2021-12-15 23:49:04 +0500

    Генерация массива вынесена в метод

--- 22Tsk/Program.cs
+++ 22Tsk/Program.cs
@@ -20,12 +20,22 @@ namespace _22Tsk
         static int sum=0;
         static int max=0;
         //Метод сумирования массива с помощью LINQ
-        static void Summ()
+        static void Array()
+        {
+            array = new int[r];
+            Random random = new Random();
+            for (int i = 0; i < r; i++)
+            {
+                array[i] = random.Next(g1, g2);
+                Console.Write(array[i] + "  ");
+            }
+        }
+        static void Summ(Task task1)
         {
             sum += array.Sum();
         }
         //Метод поиска максимального числа в массивес помощью LINQ
-        static void Maxx(Task task)
+        static void Maxx(Task task2)
         {
             max = array.Max();
         }
@@ -39,20 +49,23 @@ namespace _22Tsk
             g1 = Convert.ToInt32(Console.ReadLine()); 
             Console.WriteLine("Введите границу 2 рандомизации");
             g2 = Convert.ToInt32(Console.ReadLine());
+            //Задача на генерацию и вывод массива
+            Task tsk0 = new Task(Array);
+            tsk0.Start();
             //Создание и вывод массива случайных чисел
-            array = new int[r];
+            /*array = new int[r];
             Random random = new Random();
             for (int i = 0; i < r; i++)
             {
                 array[i] = random.Next(g1, g2);
                 Console.Write(array[i] + "  ");
-            }
+            }*/
             //Задаем задачи на выполнение
-            Action act1 = new Action(Summ);
-            Task tsk1 = new Task(act1);
+            Action<Task> act1 = new Action<Task>(Summ);
+            Task tsk1 = tsk0.ContinueWith(act1);
             Action<Task> act2 = new Action<Task>(Maxx);
             Task tsk2 = tsk1.ContinueWith(act2);
-            tsk1.Start();
+            tsk1.Wait();
             tsk2.Wait();
             Console.WriteLine();
             Console.WriteLine("Сумма элементов массива {0}", sum);
